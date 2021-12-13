9d48b3b2c2fe49c3ca5abb0dae83b5bf8d7ea458
Víctor Illanes <villanes9@gmail.com>
2021-12-13 06:09:59 -0300

    added scroll in results

--- src/Components/Movieresults/index.js
+++ src/Components/Movieresults/index.js
@@ -5,16 +5,15 @@ import './movieresults.css';
 function Movieresults(props){
 
     const set= new Set( props.movie.map( JSON.stringify ) )
-    const arrSinDuplicaciones = Array.from(set).map(JSON.parse);
+    const arrayMoviesWithoutDuplications = Array.from(set).map(JSON.parse);
+    //En el resultado se eliminan los registros duplicados
     
     return(
 
-     <section className="container__results">
-            <div className="containter__results--item">
-                <div className='containerResults'>
-                {props.searchValue!=="" && arrSinDuplicaciones.map(movies=>props.onRender(movies))}  
-                </div>
-            </div>           
+     <section className="carouselSearch">            
+                <div className='carouselSearchContainer'>
+                {props.searchValue!=="" && arrayMoviesWithoutDuplications.map(movies=>props.onRender(movies))}  
+                </div>                   
      </section>
     );
 
